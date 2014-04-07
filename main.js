140e84092b2da5a5fecc933e51f87bc214a99778
Dan Perata <djperata@aol.com>
2014-04-07 12:40:28 -0600

    practice with js and concatenation

---
+++ main.js
@@ -0,0 +1,10 @@
+var word = prompt("Enter any word");
+alert("You entered: " + word + "\n" + 
+	word + " Contains " + word.length + " characters" + "\n" +
+	"the third character in your word is " + word[2] + "\n" +
+	"your word in lowercase: " + word.toLowerCase() + "\n" +
+	"Your word in uppercase: " + word.toUpperCase() + "\n" +
+	"Example: Hello have a nice day!" + "\n" +
+	"Subword: Hell"
+
+	);
